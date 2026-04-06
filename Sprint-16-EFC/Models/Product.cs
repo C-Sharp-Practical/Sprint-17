016f64c038f60d86fdadde4e140370d8f65db4ea
Loki22978964 <lokim3059@gmail.com>
2026-04-06 14:14:37 +0300

    Added .gitignore and fixed folder structure

---
+++ Sprint-16-EFC/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EFC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+    }
+}
---
+++ Sprint-16-EFC/Models/Supermarket.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EFC.Models
+{
+    public class Supermarket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
