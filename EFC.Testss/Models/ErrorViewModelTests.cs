dfb538028c3a07236234af710b0883ed6e0d1d6a
Yurii Demyan <yra.demyan@gmail.com>
2026-04-06 18:16:00 +0300

    Task 3: Added 27 model unit tests

---
+++ EFC.Testss/Models/ErrorViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EFC.Models;
+
+namespace EFC.Tests.Models
+{
+    [TestClass]
+    public class ErrorViewModelTests
+    {
+        [TestMethod]
+        public void ErrorViewModel_Creation_ShouldReturnNotNull() => Assert.IsNotNull(new ErrorViewModel());
+
+        [TestMethod]
+        public void ErrorViewModel_RequestId_ShouldSetAndGet()
+        {
+            var error = new ErrorViewModel { RequestId = "999-ABC" };
+            Assert.AreEqual("999-ABC", error.RequestId);
+        }
+
+        [TestMethod]
+        public void ErrorViewModel_ShowRequestId_ShouldReturnTrueWhenNotEmpty()
+        {
+            var error = new ErrorViewModel { RequestId = "999-ABC" };
+            Assert.IsTrue(error.ShowRequestId);
+        }
+
+        [TestMethod]
+        public void ErrorViewModel_ShowRequestId_ShouldReturnFalseWhenNull()
+        {
+            var error = new ErrorViewModel { RequestId = null };
+            Assert.IsFalse(error.ShowRequestId);
+        }
+    }
+}
---
+++ EFC.Testss/Models/OrderDetailTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EFC.Models;
+
+namespace EFC.Tests.Models
+{
+    [TestClass] 
+    public class OrderDetailTests
+    {
+        [TestMethod] 
+        public void OrderDetail_Creation_ShouldReturnNotNull() => Assert.IsNotNull(new OrderDetail());
+
+        [TestMethod]
+        public void OrderDetail_SetAndGetOrderId_ShouldWorkCorrectly()
+        {
+            var detail = new OrderDetail { OrderId = 10 };
+            Assert.AreEqual(10, detail.OrderId);
+        }
+
+        [TestMethod]
+        public void OrderDetail_SetAndGetProductId_ShouldWorkCorrectly()
+        {
+            var detail = new OrderDetail { ProductId = 3 };
+            Assert.AreEqual(3, detail.ProductId);
+        }
+
+        [TestMethod]
+        public void OrderDetail_SetAndGetQuantity_ShouldWorkCorrectly()
+        {
+            var detail = new OrderDetail { Quantity = 5 }; 
+            Assert.AreEqual(5, detail.Quantity);
+        }
+
+        [TestMethod]
+        public void OrderDetail_UpdateQuantity_ShouldChangeValue()
+        {
+            var detail = new OrderDetail { Quantity = 1 };
+            detail.Quantity = 10;
+            Assert.AreEqual(10, detail.Quantity);
+        }
+
+        [TestMethod]
+        public void OrderDetail_UpdateOrderId_ShouldChangeValue()
+        {
+            var detail = new OrderDetail { OrderId = 1 };
+            detail.OrderId = 99;
+            Assert.AreEqual(99, detail.OrderId);
+        }
+
+        [TestMethod]
+        public void OrderDetail_UpdateProductId_ShouldChangeValue()
+        {
+            var detail = new OrderDetail { ProductId = 2 };
+            detail.ProductId = 88;
+            Assert.AreEqual(88, detail.ProductId);
+        }
+    }
+}
